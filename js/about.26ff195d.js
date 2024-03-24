1f40f28bd97c4a7a1c6411e22a0fe4af97bb07bb
Super-TD <103989503+Super-TD@users.noreply.github.com>
2024-03-24 15:54:54 +0800

    Add files via upload

---
+++ js/about.26ff195d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwy_game"]=self["webpackChunkwy_game"]||[]).push([[443],{7381:function(n,a,e){e.r(a),e.d(a,{default:function(){return l}});var u=e(3396);const t={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,u.wg)(),(0,u.iD)("div",t,s)}var i=e(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.26ff195d.js.map
